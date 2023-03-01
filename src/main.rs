b2bbc198b9440bb95ec9b4075ad41e4162384938
dave@phobos <orville.wrightt@gmail.com>
2023-03-01 13:57:26 -0800

    repo init first commit

---
+++ src/main.rs
@@ -0,0 +1,50 @@
+use rand::Rng;
+
+fn main() {
+    println!("========== start ===========");
+    // gen a rand num, create vector @ len of rand_num
+    // 
+
+    // let vcx: u8 = 0;
+    let rndlen = rand::thread_rng().gen_range(3..=25);
+    // let mut vect1 = vec![0; rndlen];
+    let mut vect1: Vec<u8> = vec![];
+
+    println!("Chosen random vector len: {}", rndlen);
+
+    // cycle through vector and populate with random data
+    for i in 1..rndlen {
+    //for i in &vect1 {
+	let rpopulate = rand::thread_rng().gen_range(0..=100);
+	println!("populating vector: {0}/{1}", i, rpopulate);
+	vect1.push(rpopulate);
+    }
+
+
+    println!("Vector len: {}", vect1.len() );
+
+    // and odd way to access the data in a vector
+    for value in &vect1 {
+        println!("Vector:<{}>", &value);
+    }
+
+
+    // playing with checking the len
+    if vect1.len() > 5 {
+        println!("List is longer than five items");
+    }
+
+    // Pattern matching on len
+    match vect1.len() {
+        0 => println!("Empty - NO data"),
+        1 => println!("Small - ONE value"),
+        2..=5 => println!("Medium - TWO ~ FIVE values"),
+        6..=15 => println!("Big - 6 ~ 15 values"),
+        _ => println!("Verg big - Many values exceeds 15"),
+    };
+
+    // while loop with predicate and pattern matching using let
+    while let Some(value) = vect1.pop() {
+        println!("value = {value}"); // using curly braces to format a local variable
+    }
+}
