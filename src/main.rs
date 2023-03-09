1ace68022f6871682d1c059dc625ad815654dfb7
Orville Wright <orville.wright@yahoo.com>
2023-03-09 12:58:48 -0800

    tidyup & beautify

--- src/main.rs
+++ src/main.rs
@@ -1,40 +1,40 @@
 use rand::Rng;
 
 fn main() {
-    println!("========== start ===========");
+    println!("========== start phase 1 ===========");
 
     /* gen a rand num, create vector @ len of rand_num
       gen a 2nd rand num and use it to populate every cell with same num during init */
     let mut rng = rand::thread_rng();
     let y: u8 = rng.gen();
     let rndlen = rand::thread_rng().gen_range(3..=25);
-    let mut vect1: Vec<u8> = vec![y; rndlen];
+    let mut vect1: Vec<u8> = vec![y; rndlen];       // crate new vector @ rndlen, cells filled with default values = y
     println!("Chosen random vector len of: {}", rndlen);
     
-
-    // load each cell in the vector with a set of random data
-    println!("\nPopulating vector with ORIGNAL random data...");
-    let mut neednl: u8 = 1;
+    /* when the vector was created, the create loaded all cells with the same value.
+       that crappy. So now we want to update/overwrite each cell with unique + random data.
+    */
+    println!("========== start phase 2 ===========");
+    println!("\nPopulating vector with NEW random data...");
+    let mut neednl: u8 = 1;     // new to print a NL after the 5th data element
     for value in &mut vect1 {
-        let y: u8 = rng.gen();
-        *value = y;     // insert random number into this cell via *derefencing
-        match neednl {      // match every 5 cycles
+        let y: u8 = rng.gen();  // generate random num
+        *value = y;             // insert random number into this cell via *derefencing
+        match neednl {          // match every 5 cycles, sets-up 5 nice columns for printing
             5 | 10 | 15 | 20 | 25 => println!("/ Vector: <{:03}>", &value),     // print rows with 5 collumns only
 	    _ => print!("Vector: <{:03}> ", &value),
 	};
-	neednl += 1;
+	neednl += 1;                // column counter
     }
     
-   println!("\n============= ==========");
-
+   println!("\n============= phase 3 ==========");
 
     // OLD learning method
     // cycle through vector and populate with random data
-    // I really dont like this methodology
-    /* for i in 1..rndlen {
+    /* save as ref for old way the solve the problem...
+    for i in 1..rndlen {
        for i in &vect1 {
-       while let Some(thing) = vect1.get(1) {    // this did not gen an error */
-    /*
+       while let Some(thing) = vect1.get(1) {    // this did not gen an error
     let mut vcx: usize = 0;     // simple counter to track which vector vell we're working on - messy/sloppy
     println!("Populate vector cells with new random data @ Vector length: {0}", vect1.len() );
     for valv in &mut vect1 {    // valv <= contents of vector cell
@@ -49,36 +49,37 @@ fn main() {
     // avoids sloppy unintegrated counters
     for (i, item) in vect1.iter_mut().enumerate() {
         let rpopulate = rand::thread_rng().gen_range(0..=100);     // select a random num
-        print!("NEW sliced vector populate - cell: {:03} / orig: {:03} / NEW: {:03}", i, item, rpopulate);
+        print!("Slice vector to populate new data - cell: {:03} / OLD: {:03} / NEW: {:03}", i, item, rpopulate);
         *item = rpopulate;      // deref item as new value
         println!(" ...updated!")
     }
 
-    println!("==============================");
+    println!("\n============= phase 4 ==========");
     println!("Vector len: {}", vect1.len() );
 
-    // and odd way to access the data in a vector
-    // this is a read operation, not a write
-    for value in &vect1 {              // a &refernce borrow, i.e. no ownership
+    // Simple basic loop to access/read the data in a vector/ No iters
+    for value in &vect1 {                  // a &refernce borrow, i.e. no ownership
         println!("Vector: <{:03}>", &value);    // &value is a borrowed &referecne -> the data vect1 points to
     }
 
-
     // playing with checking the len
+    println!("\n============= phase 5 ==========");
     if vect1.len() > 5 {
-        println!("List is longer than five items");
+        println!("Vector random length is longer than FIVE items...");
     }
 
     // Pattern matching on len
+    print!("Vector capcity is... ");
     match vect1.len() {
         0 => println!("Empty - NO data"),
-        1 => println!("Small - ONE value"),
+        1 => println!("Small - only ONE value"),
         2..=5 => println!("Medium - TWO ~ FIVE values"),
         6..=15 => println!("Big - 6 ~ 15 values"),
-        _ => println!("Verg big - Many values exceeds 15"),
+        _ => println!("Verg big - many values exceeding 15"),
     };
 
     // while loop with predicate and pattern matching using let
+    println!("\n============= phase 6 ==========");
     while let Some(value) = vect1.pop() {
         println!("value = {:03}", value); // using curly braces to format a local variable
     }
